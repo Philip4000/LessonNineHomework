7a3fe851d201dbe19ce512fb2eb1c2269253505d
Philip4000 <6427676@bk.ru>
2021-04-27 20:17:29 +0300

    Initial Commit

---
+++ LessonNineHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonNineHomework
+//
+//  Created by Philip Noskov on 27.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
